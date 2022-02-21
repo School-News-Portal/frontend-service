aefc5ddb34116286d854bc7fa62214ef8d07962f
oreste-abizera <oresteabizera11@gmail.com>
2022-02-22 01:03:28 +0200

    chore: hero icons

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import backgroundImage from "../assets/images/bgHero.png";
+import { FaCalendarAlt, FaComments, FaEye } from "react-icons/fa";
 
 export default function Hero() {
   return (
     <div
-      className="flex flex-col gap-2 items-center justify-center"
-      style={{ height: "60vh", backgroundImage: `url(${backgroundImage})` }}
+      className="flex flex-col gap-2 items-center justify-center relative"
+      style={{ height: "65vh", backgroundImage: `url(${backgroundImage})` }}
     >
       <button className="bg__primary p-3 px-12 rounded text-white">
         Hi-Tech
@@ -16,11 +17,28 @@ export default function Hero() {
       >
         Certainty listening no behaviour existence assurance situation
       </p>
-      <div className="flex gap-2 text-white text-2xl">
+      <div className="flex gap-6 text-white text-2xl mt-4">
         <p>Fredson</p>
-        <p>Jan / 15 / 2022</p>
-        <p>127</p>
-        <p>301</p>
+        <p className="flex items-center gap-2">
+          <FaCalendarAlt></FaCalendarAlt> Jan / 15 / 2022
+        </p>
+        <p className="flex items-center gap-2">
+          <FaComments></FaComments> 127
+        </p>
+        <p className="flex items-center gap-2">
+          <FaEye></FaEye> 301
+        </p>
+      </div>
+      <div className="absolute bottom-2 left-0 w-full flex items-center justify-center gap-4">
+        {Array.from(new Array(5)).map((u, index) => {
+          return (
+            <div
+              className={`text-white h-4 w-4 rounded-full cursor-pointer ${
+                index === 0 ? "bg__primary" : "bg-gray-100"
+              }`}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
