82e83e2e9722be1c48cd26630bf047d78f859b15
oreste-abizera <oresteabizera11@gmail.com>
2022-03-01 06:58:44 +0200

    feat: register ui

---
+++ pages/register.js
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import React from "react";
+import loginBg from "../assets/images/loginBg.png";
+import logo from "../assets/images/logo.png";
+
+export default function Register() {
+  return (
+    <div
+      className="min-h-screen w-screen flex items-center justify-center"
+      style={{
+        backgroundImage: `url(${loginBg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%",
+        backgroundPosition: "fixed"
+      }}
+    >
+      <div className="w-1/2">
+        <div className="bg-white rounded-3xl w-full flex flex-col items-center justify-center py-8">
+          <Image
+            src={logo}
+            alt=""
+            width="200"
+            height="100"
+            objectFit="contain"
+          ></Image>
+          <h1 className="font-bold" style={{ color: "#4D4F5C" }}>
+            CREATE ACCOUNT
+          </h1>
+
+          <div className="w-3/4 mb-8 mt-8">
+            <label className="block pl-4" style={{ color: "#4D4F5C" }}>
+              Name
+            </label>
+            <input
+              autocomplete="off"
+              style={{ border: "1px solid #E8E8E8" }}
+              className="rounded-full p-3 px-4 mt-2 w-full outline-none"
+              type="text"
+            ></input>
+          </div>
+          <div className="w-3/4 mb-8 mt-8">
+            <label className="block pl-4" style={{ color: "#4D4F5C" }}>
+              Email
+            </label>
+            <input
+              autocomplete="off"
+              style={{ border: "1px solid #E8E8E8" }}
+              className="rounded-full p-3 px-4 mt-2 w-full outline-none"
+              type="email"
+            ></input>
+          </div>
+          <div className="w-3/4">
+            <label className="block pl-4" style={{ color: "#4D4F5C" }}>
+              Password
+            </label>
+            <input
+              autocomplete="off"
+              style={{ border: "1px solid #E8E8E8" }}
+              className="rounded-full p-3 px-4 mt-2 w-full outline-none"
+              type="password"
+            ></input>
+          </div>
+
+          <div className="flex justify-between w-3/4 mt-4">
+            <p>Remember Me</p>
+            <p className="text__primary underline">Forgot Password</p>
+          </div>
+
+          <button className="mt-8 mb-4 bg__primary w-3/4 rounded-full text-white p-3 px-4">
+            REGISTER
+          </button>
+          <div className="flex gap-2">
+            <p>Already have an account?</p>
+            <p className="text__primary underline cursor-pointer">Login</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
